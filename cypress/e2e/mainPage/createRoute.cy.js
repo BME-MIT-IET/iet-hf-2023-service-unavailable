b6099a76d6709c6ea6d832bd97215cafc0f3d2da
J0NAS12 <99606538+J0NAS12@users.noreply.github.com>
2023-05-15 20:20:42 +0200

    főoldal UI tesztek

--- cypress/e2e/mainPage/createRoute.cy.js
+++
@@ -1,9 +0,0 @@
-describe('createRoute', () => {
-    it('user should be able to create a new route', () => {
-      cy.visit('http://localhost:3000/')
-
-      cy.get('#newRouteBtn').click()
-
-      cy.url().should('equal', 'http://localhost:3000/routes/new')
-    })
-  })
